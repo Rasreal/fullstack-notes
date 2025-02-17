ac88de69a940314259c6c5a116341e2c8cdf0f56
Rasreal <ersultan.t16@gmail.com>
2025-02-17 23:44:34 +0500

    feat: added a main page (ui only)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    // Dummy notes
+    const [notes, setNotes] = useState([
+        { id: 1, title: "Birinshi eske alu", content: "Bwl birinshi eske alu jazbasy." },
+        { id: 2, title: "Ekinshi eske alu", content: "Bwl ekinshi eske alu jazbasy." },
+        { id: 3, title: "Üshinshi eske alu", content: "Bwl üshinshi eske alu jazbasy." },
+    ]);
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log("title: ", title);
+        console.log("desc: ", description);
+
+        // Adding a new note
+        const newNote = {
+            id: notes.length + 1,
+            title,
+            content: description
+        };
+        setNotes([...notes, newNote]);
+
+        // Clear input fields
+        setTitle('');
+        setDescription('');
+    };
+
+    return (
+        <div className="app-container">
+            <form className="app-form" onSubmit={handleSubmit}>
+                <input
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Atau"
+                    required
+                />
+                <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Jazba"
+                    rows={10}
+                    required
+                />
+                <button type="submit">Jiberu</button>
+            </form>
+
+            {/* Display Notes */}
+            <div className="notes-grid">
+                {notes.map((note) => (
+                    <div key={note.id} className="notes-item">
+                        <div className="notes-header">
+                            <button>X</button>
+                        </div>
+                        <h2>{note.title}</h2>
+                        <p>{note.content}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
